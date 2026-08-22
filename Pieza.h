63c990c4ddaddcb231e7e024b1645ab984ab24d6
YMC-2006 <yuliana.mora.cedeno@est.una.ac.cr>
2026-08-22 07:29:06 -0600

    Primeros botones y hover

---
+++ Pieza.h
@@ -0,0 +1,9 @@
+#ifndef PIEZA_H
+#define PIEZA_H
+
+struct Pieza {
+	int x;
+	int y;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,22 +1,68 @@
 #include "raylib.h"
 
 int main(void){
-
+	
 	const int screenWidth = 1400;
-	const int screenHeight = 1000;
-	InitWindow(screenWidth, screenHeight, "Ejemplo Basico Raylib 4.0.0");
+	const int screenHeight = 800;
 	
-
-	SetTargetFPS(60);
+	InitWindow(screenWidth, screenHeight, "Inicio Tetris");
 	
-	while (!WindowShouldClose()){
-
+	Rectangle btnJugar = {600, 500, 200, 60};
+	Rectangle btnCreditos = {600, 600, 200, 60};
+	Rectangle btnMejoresPts = {570, 700, 290, 60};
+	
+	while (!WindowShouldClose())
+	{
+		
+		Vector2 mouse = GetMousePosition();
+		if(CheckCollisionPointRec(mouse, btnJugar)&& IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+			TraceLog(LOG_INFO, "Jugar Presionado");
+		}
+		
+		if (CheckCollisionPointRec(mouse, btnCreditos) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+			TraceLog(LOG_INFO, "Creditos presionado");
+		}
+		
+		if(CheckCollisionPointRec(mouse, btnMejoresPts) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+			TraceLog(LOG_INFO, "Mejores pts presionado");
+		}
+		
+	
 		BeginDrawing();
+		
 		ClearBackground(RAYWHITE);
 		
-		DrawText("Hola, esto es raylib 4.0.0!", 190, 200, 20, LIGHTGRAY);
-		DrawCircle(screenWidth / 2, screenHeight / 2 - 40, 50, MAROON);
+		DrawRectangleRec(btnJugar, BLUE);
+		DrawRectangleRec(btnCreditos, BLUE);
+		DrawRectangleRec(btnMejoresPts, BLUE);
 		
+		
+		if (CheckCollisionPointRec(mouse, btnJugar)){
+			DrawRectangleRec(btnJugar, DARKBLUE);
+		}
+		else{
+			DrawRectangleRec(btnJugar, BLUE);
+		}
+		DrawText("Jugar", 660, 520, 20, WHITE);
+		
+		
+		
+		if (CheckCollisionPointRec(mouse, btnCreditos)){
+			DrawRectangleRec(btnCreditos, DARKBLUE);
+		}
+		else{
+			DrawRectangleRec(btnCreditos, BLUE);
+		}
+		DrawText("Creditos", 660, 620, 20, WHITE);
+		
+		
+		if(CheckCollisionPointRec(mouse, btnMejoresPts)){
+			DrawRectangleRec(btnMejoresPts, DARKBLUE);
+		}else{
+			DrawRectangleRec(btnMejoresPts, BLUE);
+		}
+		DrawText("Mejores Puntuaciones", 600, 720, 20, WHITE);
+		//                                x    y   font size
 		EndDrawing();
 	}
 	
